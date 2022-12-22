bc1be6ffa25f2e444e077492c5588dd8c1d6d673
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-22 17:01:12 -0300

    :construction: feat: traversing the DOM| solution 1

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Usando seletores
+
+
+// modificando o DOM
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+     const question = e.currentTarget.parentElement.parentElement
+     question.classList.toggle("show-text");
+    });
+});
