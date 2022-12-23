d14c2fca1d1b3d0644780814a07b1e33cd0b4642
lilian-rangel <lilianrm.nutri@gmail.com>
2022-12-23 09:50:37 -0300

    :construction: feat: using selectors| solution 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,30 @@
 //Usando seletores
 
+const questions = document.querySelectorAll(".question");
 
-// modificando o DOM
+questions.forEach(function(question){
+    const btn = question.querySelector(".question-btn");
+    
+    btn.addEventListener("click", function() {
 
-const btns = document.querySelectorAll(".question-btn");
+        questions.forEach(function(item){
+            if(item!== question){
+                item.classList.remove("show-text");
+            }
+        });
+
+
+        question.classList.toggle("show-text");
+    });
+});
+
+// modificando o parentElement
+
+/*const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function(btn){
     btn.addEventListener("click", function(e){
      const question = e.currentTarget.parentElement.parentElement
      question.classList.toggle("show-text");
     });
-});
+});*/
